ee7988b882e9b45b928b536725021400756836dd
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-01-30 18:31:51 +0900

    chore: 씬 저장

---
+++ My project/Assets/01.Scripts/Ingredients/IngredientsSpawner.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class IngredientsSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _ingredient;
+
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _minXPosition;
+    [SerializeField] private float _maxXPosition;
+    
+}
