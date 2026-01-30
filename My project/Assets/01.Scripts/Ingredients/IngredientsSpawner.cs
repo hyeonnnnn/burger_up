95101d230177582a18ee6eb5cb68b32cd1a6d0ba
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-01-30 18:32:19 +0900

    chore: 씬 저장

--- My project/Assets/01.Scripts/Ingredients/IngredientsSpawner.cs
+++ My project/Assets/01.Scripts/Ingredients/IngredientsSpawner.cs
@@ -5,7 +5,16 @@ public class IngredientsSpawner : MonoBehaviour
     [SerializeField] private GameObject _ingredient;
 
     [SerializeField] private float _moveSpeed;
-    [SerializeField] private float _minXPosition;
-    [SerializeField] private float _maxXPosition;
-    
+    [SerializeField] private float _minXPosition = -2;
+    [SerializeField] private float _maxXPosition = 2;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+
+    }
 }
